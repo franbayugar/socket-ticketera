16e8d425ced45dd361450be332c3d1e235663f0a
franco <franbayu_96@hotmail.com>
2022-01-31 10:48:23 -0300

    se inserta mysql al proyecto

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 require('dotenv').config();
+const mysql = require('mysql');
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'muni_turnos',
+    user: 'root',
+    password: ''
+});
+
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('DB ON');
+    }
+
+});
+
+conexion.end();
+
 
 const Server = require('./models/server');
 
